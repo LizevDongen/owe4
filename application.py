900feeb5f8e61e55f4efa1ea6dfde3b61c4cbb03
AnnemiekeSchonthaler1 <43265496+AnnemiekeSchonthaler1@users.noreply.github.com>
2019-05-09 11:53:53 +0200

    Add files via upload

--- application.py
+++ application.py
@@ -1,11 +1,20 @@
-from flask import Flask
+"""
+Basis voor een te ontwikkelen Flask applicatie
+Creation: may 9, 2019
+(c) HAN University of Applied Science
+Author: Martijn van der Bruggen
+
+Voor deployment op Azure WebApps is het noodzakelijk het bestand
+de naam application.py te geven.
+"""
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!!!'
+def index_fun():
+    return 'Dit is de routing naar de root! Aanpassing!'
 
 
 if __name__ == '__main__':
